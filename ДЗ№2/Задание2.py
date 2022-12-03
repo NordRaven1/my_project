0538d831cb5aa2ad0ea68f31a1533e00b2caa24e
NordRaven1 <gps3119261@gmail.com>
2022-12-03 14:59:53 +0300

    ДЗ№2

---
+++ ДЗ№2/Задание2.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+user = (input("Введите ваше имя\n"))
+print(f"Привет,{user}!")
---
+++ ДЗ№2/Задание4.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+import math
+r = int(input("Введите радиус круга\n"))
+print(f"Площадь круга: {math.pi*(r**2)}")
